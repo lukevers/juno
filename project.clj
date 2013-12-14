2a0a1591a480dd568d109021e7eebf30501f3eb4
Luke Evers <me@lukevers.com>
2013-12-13 19:26:33 -0500

    Start work to browse folders, doesn't chcek for files or anything yet.

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://github.com/lukevers/juno"
   :license {:name "GNU General Public License v3.0"
             :url "http://www.gnu.org/licenses/gpl-3.0.txt"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [me.raynes/fs "1.4.4"]
+                 [http-kit "2.1.13"]
+                 [compojure "1.1.6"]]
   :main ^:skip-aot juno.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/juno/core.clj
+++ src/juno/core.clj
@@ -1,7 +1,49 @@
 (ns juno.core
-  (:gen-class))
+  (:gen-class)
+  (:require [me.raynes.fs :as fs])
+  (:use org.httpkit.server
+        compojure.core))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; Define our base. The base will be overwritten by the first argument
+; by default. There has to be a base set so we know where to start
+; hosting from.
+(def base "")
+; Define our current location.
+(def loc "")
+
+; Turns seq into an html list
+(defn clist [ls]
+  (def htm "<ul>")
+  (if (not (empty? loc))
+    (if (.endsWith loc "/")
+      (def htm (str htm "<a href=\"../\"><li>..</a></li>"))
+      (def htm (str htm "<a href=\"" loc "/../\"><li>..</a></li>"))) nil)
+  (if (.endsWith loc "/")
+    (doseq [i ls] (def htm (str htm "<a href=\"" loc i "\"><li>" i "</li></a>")))
+    (doseq [i ls] (def htm (str htm "<a href=\"" loc "/" i "\"><li>" i "</li></a>"))))
+  (str htm "</ul>"))
+
+; This is where things get fun!
+(defn roots [req]
+  (def dir (str base (get req :uri)))
+  (def loc (get req :uri))
+  ; If the location is at "/" we want to set it to be an empty string.
+  (if (= loc "/") (def loc ""))
+  (def ls (fs/list-dir dir))
+  (clist ls))
+
+(defroutes router
+  (GET "*" [] roots))
+
+(defn logger [app]
+  (fn [req]
+    (println req)
+    (app req)))
+
+(def app 
+  (-> router
+      logger))
+  
+(defn -main [& args]
+  (def base (first args))
+  (run-server app {:port 8080}))
